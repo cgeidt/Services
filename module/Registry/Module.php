4f9f43e8397c09ce00dd52014fc93c76af5bb85f
cgeidt <cgeidt@web.de>
2015-01-08 15:12:27 +0100

    Added modules

---
+++ module/Registry/Module.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Zend Framework (http://framework.zend.com/)
+ *
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
+ * @license   http://framework.zend.com/license/new-bsd New BSD License
+ */
+
+namespace Registry;
+
+use Zend\Mvc\ModuleRouteListener;
+use Zend\Mvc\MvcEvent;
+use \Registry\Model\Service;
+use \Registry\Model\ServiceTable;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\TableGateway\TableGateway;
+
+class Module
+{
+    public function onBootstrap(MvcEvent $e)
+    {
+        $eventManager        = $e->getApplication()->getEventManager();
+        $moduleRouteListener = new ModuleRouteListener();
+        $moduleRouteListener->attach($eventManager);
+    }
+
+    public function getConfig()
+    {
+        return include __DIR__ . '/config/module.config.php';
+    }
+
+    public function getAutoloaderConfig()
+    {
+        return array(
+
+            'Zend\Loader\StandardAutoloader' => array(
+                'namespaces' => array(
+                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+                ),
+            ),
+        );
+    }
+
+    public function getServiceConfig()
+    {
+        return array(
+            'factories' => array(
+                'Registry\Model\ServiceTable' =>  function($sm) {
+                    $tableGateway = $sm->get('ServiceTableGateway');
+                    $table = new ServiceTable($tableGateway);
+                    return $table;
+                },
+                'ServiceTableGateway' => function ($sm) {
+                    $dbAdapter = $sm->get('Zend\Db\Adapter\Adapter');
+                    $resultSetPrototype = new ResultSet();
+                    $resultSetPrototype->setArrayObjectPrototype(new Service());
+                    return new TableGateway('service', $dbAdapter, null, $resultSetPrototype);
+                },
+            ),
+        );
+    }
+
+}
---
+++ module/Registry/src/Registry/Model/ServiceTable.php
@@ -0,0 +1,57 @@
+<?php
+namespace Registry\Model;
+
+use Zend\Db\TableGateway\TableGateway;
+
+class ServiceTable
+{
+    protected $tableGateway;
+
+    public function __construct(TableGateway $tableGateway)
+    {
+        $this->tableGateway = $tableGateway;
+    }
+
+    public function fetchAll()
+    {
+        $resultSet = $this->tableGateway->select();
+        return $resultSet;
+    }
+
+    public function getService($id)
+    {
+        $id  = (int) $id;
+        $rowset = $this->tableGateway->select(array('id' => $id));
+        if($rowset->count() == 0){
+            return null;
+        }
+        return $rowset->current();
+    }
+
+    public function saveService(Service $service)
+    {
+
+        $data = $service->getArrayCopy();
+
+        $id = (int) $service->getId();
+        if (!$id) {
+            if(!$this->getService($id)){
+                throw new \Exception('A service with the name "'.$service->getName().'" already exists');
+            }
+            $this->tableGateway->insert($data);
+            $id = $this->tableGateway->lastInsertValue;
+        } else {
+            if ($this->getService($id)) {
+                $this->tableGateway->update($data, array('id' => $id));
+            } else {
+                throw new \Exception('Service id does not exist');
+            }
+        }
+        return $id;
+    }
+
+    public function deleteService($id)
+    {
+        $this->tableGateway->delete(array('id' => (int) $id));
+    }
+}
